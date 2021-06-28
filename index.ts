212c1a43ddec22b342d68422eb870b16443d4cca
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2021-06-28 08:39:35 +0200

    Update

--- index.ts
+++ index.ts
@@ -3,6 +3,14 @@ import 'dotenv/config'
 import { Client, Intents } from './structures'
 import config from './config'
 
+import { createServer } from 'http'
+
+createServer((_req, res) => {
+    res.write('Hello')
+    res.end()
+}).listen(process.env.PORT ?? 8080)
+
+
 const client = new Client({
 	intents: [
 		Intents.FLAGS.GUILDS, 
@@ -16,3 +24,8 @@ console.log(`Loaded a ${client.load('commands')} command.`)
 console.log(`Loaded a ${client.load('events')} event.`)
 
 void client.login(config.token)
+
+process
+    .on('uncaughtException', (err) => console.error(`Uncaught Exception: ${err.stack}`))
+    .on('unhandledRejection', (error) => console.error('Unhandled Rejection', error))
+    .on('warning', (warn) => console.warn(warn))
