95447e5adefc0ac202db8cbd443f8e07e82128e0
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-04-11 07:47:28 +0000

    minor changes

--- index.ts
+++ index.ts
@@ -3,18 +3,26 @@ import { createServer } from 'http'
 import { Client, Intents } from './structures'
 import config from './config'
 
-createServer((_req, res) => {
-    res.write('Hello')
-    res.end()
-}).listen(process.env.PORT ?? 8080)
+
+const isHeroku = process.env._?.indexOf("heroku") !== -1
+const isReplIt = !!process.env.REPL_ID
+
+if (isHeroku || isReplIt) {
+    const port = process.env.PORT ?? 8080
+    console.log('Start listening on port:', port)
+    createServer((_req, res) => {
+        res.write('Pong!')
+        res.end()
+    }).listen(port)
+}
 
 
 const client = new Client({
-	intents: [
-		Intents.FLAGS.GUILDS, 
-		Intents.FLAGS.GUILD_VOICE_STATES
-	],
-	restTimeOffset: 0
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ],
+    restTimeOffset: 0
 })
 
 console.log(`Loaded a ${client.load('commands')} command.`)
